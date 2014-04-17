b93d3d538c2293768ba9864f60d286e7eca7c54b
root <root@monument>
2014-04-17 17:17:43 +0100

    Add an email

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 ./node_modules/coffee-script/bin/coffee main.coffee --reputations http://reputations.playgen.org --gaminomics http://gaminomics.playgen.org
+echo 'Reputation broke yo' | mail -s 'A gift from the gods' lex@playgen.com
