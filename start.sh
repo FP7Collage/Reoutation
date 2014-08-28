df3cbb2d2c771e2ae0fb2c0a5da521edf1700f54
Gvidas <gvidas@playgen.com>
2014-08-28 12:32:58 +0100

    No more yargs needed

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-./node_modules/coffee-script/bin/coffee main.coffee --reputations http://reputations.playgen.org --gaminomics http://gaminomics.playgen.org
+./node_modules/coffee-script/bin/coffee main.coffee
 echo 'Reputation broke yo' | mail -s 'A gift from the gods' lex@playgen.com
